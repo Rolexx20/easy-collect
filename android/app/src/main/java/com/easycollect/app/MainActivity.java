df954fedeec31099f7624224e15d2cb3e4cc9640
Rolexx20 <rolexkk7@gmail.com>
2025-06-06 09:23:43 +0000

    Revert "Android APK Generate Based reverse"
    This reverts commit 78896b4af31adc4d43327d71ee138746bca4b9fd.

--- android/app/src/main/java/com/easycollect/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.easycollect.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.easycollect.app',
-  appName: 'Easy Collect',
-  webDir: 'dist'
-};
-
-export default config;
