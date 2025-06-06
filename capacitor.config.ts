78896b4af31adc4d43327d71ee138746bca4b9fd
Rolexx20 <rolexkk7@gmail.com>
2025-06-06 09:09:27 +0000

    Android APK Generate Based reverse

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.easycollect.app',
+  appName: 'Easy Collect',
+  webDir: 'dist'
+};
+
+export default config;
