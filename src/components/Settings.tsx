f7ce5743f2dd4f457b177c809e15602648b94b9d
Kelistan K <125884143+Keliztan20@users.noreply.github.com>
2025-11-06 12:06:30 +0530

    Time Check auto 1

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -518,7 +518,7 @@ const Settings = ({ language, setLanguage }: SettingsProps) => {
 
       const now = new Date();
       const nextMidnight = new Date(now);
-      nextMidnight.setHours(12, 5, 0, 0); // next midnight
+      nextMidnight.setHours(12, 8, 0, 0); // next midnight
       const msUntilMidnight = nextMidnight.getTime() - now.getTime();
 
       // set timeout to run once at midnight, then setInterval every 24h
